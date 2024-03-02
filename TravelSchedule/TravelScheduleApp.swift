f6c02e94745c32329ee04aa529148f2d52a58a42
KudryashovAlexander <sasha080193@gmail.com>
2024-03-02 11:04:18 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Александр Кудряшов on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
